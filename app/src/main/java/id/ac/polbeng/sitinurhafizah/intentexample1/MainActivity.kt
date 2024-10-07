3c079ca75c9b7a23d637286425b174b55ab5e72d
nurhafizahsiti <nurhafizahsiti27@gmail.com>
2024-10-07 18:48:49 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/polbeng/sitinurhafizah/intentexample1/MainActivity.kt
@@ -0,0 +1,34 @@
+package id.ac.polbeng.sitinurhafizah.intentexample1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.polbeng.sitinurhafizah.intentexample1.databinding.ActivityMainBinding
+import java.util.logging.Logger
+class MainActivity : AppCompatActivity() {
+    private val Log = Logger.getLogger(MainActivity::class.java.name)
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.info("onCreate")
+        binding.btnToSecondActivity.setOnClickListener {
+            val intent = Intent(this@MainActivity,
+                SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.info("onPause")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.info("onRestart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.info("onResume")
+    }
+}
